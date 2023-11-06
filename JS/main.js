4ea865bd352746d94c82bc20b7a410786b01a81d
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2023-11-06 15:14:13 -0500

    logout y pagina de informacion

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,32 @@
+
+function logout() {
+
+
+    localStorage.removeItem("loginExitoso");
+    localStorage.removeItem("usuario");
+    location.reload();
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
 
 
-    if (localStorage.getItem('loginExitoso') === 'true') {
+    if (localStorage.getItem("loginExitoso") === "true") {
 
-        var enlace = document.querySelector('.login');
+        var enlace = document.querySelector(".login");
         var data = localStorage.getItem("usuario");
         if (enlace) {
             enlace.textContent = data;
@@ -13,6 +35,36 @@ document.addEventListener("DOMContentLoaded", function () {
                 event.preventDefault();
 
             });
+
+
+            var log = document.querySelector(".listaLogin");
+
+
+            var nuevoElemento = document.createElement("li");
+            nuevoElemento.className = "cierre";
+            var texto = document.createTextNode("Cerrar sesion ");
+            var referencia = document.createElement("a");
+            referencia.appendChild(texto);
+            referencia.href = "javascript:void(0); "
+            referencia.onclick = logout;
+            nuevoElemento.appendChild(referencia);
+
+
+            var nuevaLista = document.createElement("ul");
+
+            nuevaLista.className = "menuVertical";
+            nuevaLista.appendChild(nuevoElemento);
+            log.appendChild(nuevaLista);
+
+
+
+
+
+
+
+
+
+
         };
 
 
@@ -20,4 +72,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
     };
 });
+
 
